9d75bd0eae4dad7e8a5345db776482eb698c6f56
Kevin <60113330+facetorched@users.noreply.github.com>
2020-01-20 17:15:25 -0600

    Rename chungismod to bartech

---
+++ BarTech/src/main/java/com/facetorched/chungismod/util/Reference.java
@@ -0,0 +1,11 @@
+package com.facetorched.chungismod.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "cm";
+	public static final String NAME = "chungismod";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.facetorched.chungismod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.facetorched.chungismod.proxy.CommonProxy";
+}
